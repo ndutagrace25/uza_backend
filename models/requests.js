2a84465ef1ea6cba02ce312b99e850c5da9adcf6
solomon@mawingu <solomon.wachira@mawingunetworks.com>
2021-06-24 13:51:53 +0300

    Sequelize generated model classes, sourced from the Migration files

---
+++ models/requests.js
@@ -0,0 +1,27 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Requests extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Requests.belongsTo(models.Products,{
+        foreignKey: "product_id", as: "product"
+      })
+    }
+  };
+  Requests.init({
+    quantity: DataTypes.INTEGER,
+    product_id: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Requests',
+  });
+  return Requests;
+};
---
+++ models/users.js
@@ -0,0 +1,31 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Users extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Users.belongsTo(models.Businesses,{
+        foreignKey: "business_id", as: "business"
+      })
+    }
+  };
+  Users.init({
+    first_name: DataTypes.STRING,
+    last_name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    contact: DataTypes.INTEGER,
+    password: DataTypes.STRING,
+    business_id: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Users',
+  });
+  return Users;
+};
