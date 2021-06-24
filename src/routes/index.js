17345965db14952b8ec5d02eb0db0395bd8c1390
solomon@mawingu <solomon.wachira@mawingunetworks.com>
2021-06-24 18:36:00 +0300

    Added a file that exposes all route files to the app file

---
+++ src/routes/index.js
@@ -0,0 +1,8 @@
+const businesses = require("./businesses");
+const products = require("./products");
+const users = require("./users");
+module.exports = {
+   businesses,
+   products,
+   users
+}
