1ce038885cbee2ed1f7c3bb06deaef571b742d17
solomon@mawingu <solomon.wachira@mawingunetworks.com>
2021-06-24 18:34:46 +0300

    Added a file that exposes all controllers to be used by routes

---
+++ src/controllers/index.js
@@ -0,0 +1,7 @@
+const BusinessController = require("./BusinessController")
+const ProductController = require("./ProductController")
+const CategoryController = require("./CategoryController")
+const UserController = require("./UserController")
+const RequestController = require("./RequestController")
+//
+module.exports = {BusinessController,ProductController,CategoryController,UserController,RequestController}
