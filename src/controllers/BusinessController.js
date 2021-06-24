1a7e032d951b62658da94e3e8aebc7cc3074f11e
solomon@mawingu <solomon.wachira@mawingunetworks.com>
2021-06-24 18:33:16 +0300

    Added delete and edit functionality to business entity

--- src/controllers/BusinessController.js
+++ src/controllers/BusinessController.js
@@ -19,7 +19,7 @@ module.exports = {
             result({Error: error},null)
         }
     },
-    getBusiness(){
+    getBusiness(result){
         Business.findAll({
             attributes: ['id','name','slogan','weblink','location','address']
         }).then(found=>{
@@ -27,9 +27,38 @@ module.exports = {
         }).catch(error=>result({Error: error},null))
     },
     editBizInfo(bizInfo,result){
-
+        const {error,isValid} = validator.edit(bizInfo)
+        if(isValid){
+            //Validate id provided
+            Business.findByPk(bizInfo.biz_id).then(bizfound=>{
+                if(bizfound!==null){
+                    bizfound.update({
+                        name: bizInfo.name,
+                        slogan: bizInfo.slogan,
+                        weblink: bizInfo.weblink,
+                        location: bizInfo.location,
+                        address: bizInfo.address
+                    }).then(()=>{
+                        result(null,{Message: "Update Successful"})
+                    }).catch(err=>result({Error: err},null))
+                }else{
+                    result({Error: "Invalid Business Provided"},null)
+                }
+            }).catch(err=>result({Error: err},null))
+        }else{
+            result({Error: error},null)
+        }
     },
-    deleteBiz(bizId){
-        
+    deleteBiz(bizId,result){
+        //Ensure id is valid
+        Business.findByPk(bizId).then(found=>{
+            if(found!==null){
+                found.destroy().then(()=>{
+                    result(null, {Message: "Business Deletion Successful"})
+                }).catch(err=>result({Error: err},null))
+            }else{
+                result({Error: "Invalid business provided"},null)
+            }
+        }).catch(error=>result({error:error},null))
     }
 }
